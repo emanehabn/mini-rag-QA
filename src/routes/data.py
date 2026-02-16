847f3d221328f8b45977490f63cccf00f2fa7c5d
Eman-Ehab <emanehab.ieee@gmail.com>
2026-02-16 16:40:51 +0200

    perf: return file upload

--- src/routes/data.py
+++ src/routes/data.py
@@ -35,7 +35,7 @@ async def upload_data(project_id: str, file: UploadFile,\
         )
     project_dir_path = ProjectController().get_project_path(project_id=project_id)
     #file_path = os.path.join(project_dir_path, file.filename)
-    file_path = data_controller.generate_unique_filepath(
+    file_path, file_id= data_controller.generate_unique_filepath(
         orig_file_name=file.filename,
         project_id=project_id
     )
@@ -54,6 +54,7 @@ async def upload_data(project_id: str, file: UploadFile,\
 
     return JSONResponse(
         content={
-            "signal": ResponseSignals.FILE_UPLOAD_SUCCESS.value
+            "signal": ResponseSignals.FILE_UPLOAD_SUCCESS.value,
+            "file_id": file_id
         }
     )
